872c1507f7a28ae861e911e6b922e5c4ecc01d4f
leokey47 <leokeyplay22@gmail.com>
2025-03-28 18:17:57 +0200

    Добавьте файлы проекта.

---
+++ market_api/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Настройка CORS для всех нужных портов
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+        builder.WithOrigins("http://localhost:3001", "https://localhost:3001")  // Разрешаем оба протокола
+               .AllowAnyMethod()  // Разрешаем все методы (GET, POST и т.д.)
+               .AllowAnyHeader()  // Разрешаем все заголовки
+               .AllowCredentials()); // Разрешаем использование кук и аутентификацию
+});
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Включаем CORS
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
